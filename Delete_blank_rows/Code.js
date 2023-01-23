87350ddd421aa7c3d151d50aa344fa875819b871
productivesheets <122878810+productivesheets@users.noreply.github.com>
2023-01-23 15:04:37 +0530

    Update Code.js

--- Delete_blank_rows/Code.js
+++ Delete_blank_rows/Code.js
@@ -1,1 +1,23 @@
+//Code Developed by Manideep Chaudhary from http://productivesheets.com
+function removeEmptyRows() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  const activeRange = sheet.getActiveRange().getValues();
+  let rowIndex = sheet.getActiveRange().getRowIndex();
+  const colIndex = sheet.getActiveRange().getColumn();
+  const numColumns = sheet.getActiveRange().getNumColumns();
 
+  activeRange.forEach((item, index) => {
+  if (item.every(element => element.toString().trim().length === 0)) {
+    sheet.getRange(index + rowIndex, colIndex, 1, numColumns).deleteCells(SpreadsheetApp.Dimension.ROWS);
+    rowIndex--;
+  }
+  });
+}
+
+function onOpen() {
+  let ui = SpreadsheetApp.getUi();
+  ui.createMenu('Custom Removes')
+    .addItem('Remove empty rows', 'removeEmptyRows')
+    .addToUi();
+  }
+}
